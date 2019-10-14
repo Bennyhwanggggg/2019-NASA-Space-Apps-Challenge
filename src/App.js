fc38504ad128e71346b08b2dcf67515b96888054
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-10-14 20:41:23 +1100

    clea app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Canvas, useThree } from 'react-three-fiber';
+import React from 'react';
+import { Canvas } from 'react-three-fiber';
 import Planet from './components/Planet';
 import * as THREE from 'three';
 import { Light } from './components/Light';
@@ -9,15 +9,11 @@ function App() {
     const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(5, 5, 5);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
-    
-    const [cam, setCam] = useState(camera);
-
-    const { gl } = useThree()
 
     return (
         <div className="main">
             <Canvas 
-                camera={cam}
+                camera={camera}
                 onCreated={({ gl }) => {
                     gl.shadowMap.enabled = true
                     gl.shadowMap.type = THREE.PCFSoftShadowMap
