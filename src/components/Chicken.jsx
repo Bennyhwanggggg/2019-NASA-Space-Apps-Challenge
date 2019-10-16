fddc3871a9df2b724e117f951c8d4619dc744066
shcngsc <shcngsc@gmail.com>
2019-10-16 21:19:32 +1100

    blala moli liliang

--- src/components/Chicken.jsx
+++ src/components/Chicken.jsx
@@ -7,9 +7,25 @@ export const Chicken = props => {
 
     const [model, setModel] = useState();
 
+    const playAnimation = () => {
+        const mixer = new THREE.AnimationMixer(model.scene);
+        const { animations: clips } = model;
+
+        clips.forEach(clip => {
+            mixer.clipAction(clip).play();
+        });
+    }
+
     useEffect(() => {
         new GLTFLoader().load(modelURL, setModel);
     }, [])
+
+    useEffect(() => {
+        if (!model) return;
+
+        playAnimation();
+    }, [model]);
+
 
     return model ? <primitive 
                         object={model.scene} 
