f2d184d8e88217f5991a84537de35a52392e2a83
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-10-19 14:49:26 +1100

    refactor background

--- frontend/src/TwoDView.js
+++ frontend/src/TwoDView.js
@@ -3,7 +3,8 @@ import { Stage, } from '@inlet/react-pixi';
 import Background from './components/Background';
 import AnimatableObjects from './components/AnimatableObjects';
 import ImageUploadButton from './components/ImageUploadButton';
-import * as PIXI from 'pixi.js'
+import * as PIXI from 'pixi.js';
+import { Viewport } from 'pixi-viewport';
 import chicken1 from './assets/chicken1.jpeg';
 import chicken2 from './assets/chicken2.jpeg';
 import chicken3 from './assets/chicken3.png';
@@ -21,33 +22,47 @@ texturesArray.push(temp2);
 texturesArray.push(temp3);
 texturesArray.push(dogggy);
 
-class TwoDView extends React.Component {
+function TwoDView() {
+    // const viewPort = new Viewport({
+    //     screenWidth: window.innerWidth,
+    //     screenHeight: window.innerHeight,
+    //     worldWidth: 1000,
+    //     worldHeight: 1000// the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+    // });
 
-    render() {
-        return (
-            <div>
-                <Stage width={720} 
-                    height={1280} 
-                    options={{
-                        autoStart: true,
-                        backgroundColor: 0xeef1f5
-                        }}
-                    style={{
-                            position: 'absolute', left: '50%', top: '50%',
-                            transform: 'translate(-50%, -50%)'
-                        }}
-                >
-                    <Background/>
-                    <AnimatableObjects
-                        textures={texturesArray}
-                        x={320}
-                        y={680}
-                    />
-                </Stage>
-                <ImageUploadButton/>
-            </div>
-        )
+    const canvas = {
+        width: 2560,
+        height: 1440
     }
+
+    return (
+        <div>
+            <Stage 
+                width={canvas.width} 
+                height={canvas.height} 
+                options={{
+                    autoStart: true,
+                    backgroundColor: 0xeef1f5
+                    }}
+                style={{
+                        position: 'absolute', left: '50%', top: '50%',
+                        transform: 'translate(-50%, -50%)'
+                    }}
+                // viewPort={viewPort}
+            >
+                <Background
+                    width={canvas.width}
+                    height={canvas.height}
+                />
+                <AnimatableObjects
+                    textures={texturesArray}
+                    x={320}
+                    y={680}
+                />
+            </Stage>
+            <ImageUploadButton/>
+        </div>
+    );
 }
 
 export default TwoDView;
--- frontend/src/components/Background.js
+++ frontend/src/components/Background.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { Sprite, Container } from '@inlet/react-pixi';
 import BackgroundImage from '../assets/frog_3.jpg';
 
-class Background extends React.Component {
-    render() {
-        return (
-            <Container>
-                <Sprite
-                    // image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"
-                    image={BackgroundImage}
-                    // I think: http://pixijs.download/release/docs/PIXI.Sprite.html#anchor
-                    x={400} // anchor x
-                    y={400} // anchor y
+const Background = props => {
+
+    return (
+        <Container>
+            <Sprite
+                // image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"
+                image={BackgroundImage}
+                // I think: http://pixijs.download/release/docs/PIXI.Sprite.html#anchor
+                x={0} // anchor x
+                y={0} // anchor y
+                width={props.width} 
+                height={props.height}
                 />
-            </Container>
-        )
-    }
+        </Container>
+    )
 }
 
 export default Background
